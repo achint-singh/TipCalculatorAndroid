259e959e2cbf383dd85e9a6824faf1b3d0050ea4
Achint Singh <achintsingh@achints-mbp.dynamic.ucsd.edu>
2016-11-20 02:36:20 -0800

    Implemented Calculator Functionality

--- app/src/main/java/com/practice/tipcalculator/CalculationActivity.java
+++ app/src/main/java/com/practice/tipcalculator/CalculationActivity.java
@@ -2,6 +2,10 @@ package com.practice.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CalculationActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class CalculationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+
+        EditText initialBill = (EditText)findViewById(R.id.originalBill);
+        EditText tipPercent = (EditText)findViewById(R.id.tipPercentage);
+        TextView tipValue = (TextView)findViewById(R.id.totalTip);
+        TextView finalBill = (TextView)findViewById(R.id.totalBill);
+
+        RecomputeTextWatcher textWatcher = new RecomputeTextWatcher(initialBill, tipPercent, tipValue, finalBill);
+
+
+        initialBill.addTextChangedListener(textWatcher);
+        tipPercent.addTextChangedListener(textWatcher);
+
     }
 }
---
+++ app/src/main/java/com/practice/tipcalculator/recomputeTextWatcher.java
@@ -0,0 +1,55 @@
+package com.practice.tipcalculator;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by achintsingh on 11/20/16.
+ */
+public class RecomputeTextWatcher implements TextWatcher {
+
+    private EditText initial_Bill;
+    private EditText tip_Percent;
+    private TextView final_Tip;
+    private TextView final_Bill;
+
+    public RecomputeTextWatcher(EditText initialBill, EditText tipPercent, TextView finalTip, TextView finalBill) {
+        initial_Bill = initialBill;
+        tip_Percent = tipPercent;
+        final_Tip = finalTip;
+        final_Bill = finalBill;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        update();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+
+    public void update() {
+        double tipPercent = 0.0;
+        double initBill = 0.0;
+        try {
+            tipPercent = Double.parseDouble(tip_Percent.getText().toString());
+            initBill = Double.parseDouble(initial_Bill.getText().toString());
+        } catch (NumberFormatException ex) {
+            final_Bill.setText("Total: $0.00");
+            final_Tip.setText("Tip: $0.00");
+        }
+        double tip = initBill * tipPercent/100;
+        double total = initBill + tip;
+        final_Bill.setText("Total: $" + total);
+        final_Tip.setText("Tip: $" + tip);
+    }
+
+
+}
